ff45e5eb845dc5dbaa6a2a3eae6bc0c71705311d
jpqueirozrch <joao.rocha@brius.com.br>
2024-10-21 16:59:01 -0300

    feat: creation of base query builders

---
+++ projeto 3/code/projeto3/src/Controller/UserController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
+
+export class UserController {
+    private userRepository = AppDataSource.getRepository(User);
+
+    // GET - Fetch all users
+    async getAll(request: Request, response: Response) {
+        const users = await this.userRepository.find();
+        return response.json(users);
+    }
+
+    // POST - Create a new user
+    async create(request: Request, response: Response) {
+        const newUser = this.userRepository.create(request.body);
+        const results = await this.userRepository.save(newUser);
+        return response.json(results);
+    }
+
+    // DELETE - Remove a user
+    async delete(request: Request, response: Response) {
+        const id = request.params.id;
+        const result = await this.userRepository.delete(id);
+        return response.json(result);
+    }
+
+    // PUT - Update a user
+    async update(request: Request, response: Response) {
+        const id = request.params.id;
+        const user = await this.userRepository.findOne(id);
+        if (user) {
+            this.userRepository.merge(user, request.body);
+            const results = await this.userRepository.save(user);
+            return response.json(results);
+        }
+        return response.status(404).json({ message: 'User not found' });
+    }
+}
