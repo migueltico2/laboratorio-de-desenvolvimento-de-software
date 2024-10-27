d144cf70462507b8ebfcb5faf94f2e6807316b24
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2024-10-27 20:06:16 -0300

    feat: add local storage

--- projeto 3/code/client/src/App.vue
+++ projeto 3/code/client/src/App.vue
@@ -16,12 +16,23 @@ const currentUser = ref(null);
 const handleUserRegistered = (userData) => {
 	currentUser.value = userData;
 	isLoggedIn.value = true;
+	localStorage.setItem('userData', JSON.stringify(userData));
 };
 
 const handleLogout = () => {
 	isLoggedIn.value = false;
 	currentUser.value = null;
+	localStorage.removeItem('userData');
 };
+
+const savedLocalStorage = () => {
+	const userData = localStorage.getItem('userData');
+	if (userData) {
+		currentUser.value = JSON.parse(userData);
+		isLoggedIn.value = true;
+	}
+};
+savedLocalStorage();
 </script>
 
 <style scoped></style>
